d1636acac12dd11a6f1b40e46dc6cb3e56fa0dd4
Tibin Lukose <tibin@zycon.in>
2020-03-13 21:26:35 +0400

    Gateway news error

---
+++ src/java-apps/gateway/src/main/java/in/zycon/demo/gateway/controller/Home.java
@@ -0,0 +1,12 @@
+package in.zycon.demo.gateway.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Home {
+    public ResponseEntity<?> getAllNews(){
+
+    }
+}
---
+++ src/java-apps/gateway/src/main/java/in/zycon/demo/gateway/services/NewsService.java
@@ -0,0 +1,8 @@
+package in.zycon.demo.gateway.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NewsService {
+    public Set<News>
+}
