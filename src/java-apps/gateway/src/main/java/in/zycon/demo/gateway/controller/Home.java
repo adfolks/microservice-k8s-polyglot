138e1e58876c7930a817a2c18a5d0f2a7d66002b
Tibin Lukose <tibin@zycon.in>
2020-03-13 21:45:08 +0400

    Gateway news error

--- src/java-apps/gateway/src/main/java/in/zycon/demo/gateway/controller/Home.java
+++ src/java-apps/gateway/src/main/java/in/zycon/demo/gateway/controller/Home.java
@@ -1,12 +1,20 @@
 package in.zycon.demo.gateway.controller;
 
+import in.zycon.demo.gateway.services.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class Home {
-    public ResponseEntity<?> getAllNews(){
+    @Autowired
+    NewsService newsService;
+
+    @GetMapping("/news")
+    public ResponseEntity<?> getAllNews() {
+        return ResponseEntity.of(newsService.getAllNews());
 
     }
 }
--- src/java-apps/gateway/src/main/java/in/zycon/demo/gateway/services/NewsService.java
+++ src/java-apps/gateway/src/main/java/in/zycon/demo/gateway/services/NewsService.java
@@ -1,8 +1,28 @@
 package in.zycon.demo.gateway.services;
 
+import in.zycon.demo.hawks.models.News;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class NewsService {
-    public Set<News>
+    @Value("${news.host}")
+    String host;
+    @Value("${news.path}")
+    String path;
+
+    public Set<News> getAllNews(){
+
+        RestTemplate restTemplate = new RestTemplate();
+        return new HashSet<>(Arrays.asList(restTemplate.getForEntity(host+path,News[].class).getBody()));
+    }
+
+
+
 }
