b1e7fb80618c8ad72da94b76780bb801f7ad5dc0
Hassan R <hassan@adfolks.com>
2020-03-13 23:16:45 +0400

    running version with Dockerfile and simple readme

--- src/weather/weather/get_weather.py
+++ src/weather/weather/get_weather.py
@@ -12,9 +12,17 @@ log = logger.get_logger()
 # config = configparse.get_config()
 log.info('Configuration loaded')
 
-def get_weather():
-    querystring = {"id":str(configparse.get_city_id()),"appid":str(configparse.get_api_key())}
+def get_weather(city_id):
+    result = {}
+    if city_id == None:
+        city_id = configparse.get_city_id()
+    querystring = {"id":str(city_id),"appid":str(configparse.get_api_key())}
     response = requests.request("GET", URL, params=querystring)
-    result = json.loads(response.text)
-    result['code'] = response.status_code
+    response_json = json.loads(response.text)
+    result['cityName'] = response_json['name']
+    result['code'] = str(response.status_code)
+    result['countryName'] = response_json['sys']['country']
+    result['weatherMain'] = response_json['weather'][0]['main']
+    result['feelsLike'] = response_json['main']['feels_like']
+    result['temp'] = response_json['main']['temp']
     return result
