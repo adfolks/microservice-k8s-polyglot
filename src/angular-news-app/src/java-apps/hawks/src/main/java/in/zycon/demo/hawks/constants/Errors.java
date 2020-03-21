2e67efe418bf1e3eb0c990dab3d4a3758b7db19d
bjoe.adfolks <BJoe.adfolks@emaar.ae>
2020-03-22 00:49:26 +0400

    front-end

--- src/angular-news-app/src/java-apps/hawks/src/main/java/in/zycon/demo/hawks/constants/Errors.java
+++
@@ -1,5 +0,0 @@
-package in.zycon.demo.hawks.constants;
-
-public class Errors {
-    public final static String PARTNER_FAILED = "Error while fetching data from partner";
-}
--- src/angular-news-app/src/java-apps/news/src/main/java/in/zycon/demo/gateway/exception/NewsNotFound.java
+++
@@ -1,8 +0,0 @@
-package in.zycon.demo.gateway.exception;
-
-public class NewsNotFound extends RuntimeException {
-
-    public NewsNotFound(String message) {
-        super(message);
-    }
-}
