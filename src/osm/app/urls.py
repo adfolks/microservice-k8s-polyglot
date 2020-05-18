ae93810b794930fe7078920681e179cd79cd8381
ashmilhussain <ashmilhussain@gmail.com>
2020-05-18 22:12:04 +0530

    adding shuup code

---
+++ src/osm/app/urls.py
@@ -0,0 +1,9 @@
+
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^sa/', include('shuup.admin.urls', namespace="shuup_admin", app_name="shuup_admin")),
+    url(r'^', include('shuup.front.urls', namespace="shuup", app_name="shuup")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
