93adb962dc434e243973cd190be1f26aba8785ce
Tibin Lukose <tibin@zycon.in>
2020-09-30 01:01:22 +0400

    updates

--- src/angular/src/config.js
+++ src/angular/src/config.js
@@ -1,5 +1,5 @@
 var config = {
-    url: "https://aks-demo.liwanetworks.com/v1/all?id=292223",
+    url: "https://aks-demo.liwanetworks.com/api/v1/all?id=292223",
     cartUrl:"https://aks-demo.liwanetworks.com/api/shuup/front/products/",
     adsUrl:"https://aks-demo.liwanetworks.com:1337/case-studies"
 }
